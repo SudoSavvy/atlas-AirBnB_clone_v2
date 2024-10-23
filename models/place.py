4d6e3c5669d3db3e52f22fcc3979be9b10d850ea
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-10-23 10:26:15 -0500

    updated imports / added argument handler for amenity / updated key storage and pushthrough for place

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the place class"""
+import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
@@ -8,15 +9,16 @@ from os import getenv
 import models
 
 
-place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True,
-                             nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True,
-                             nullable=False))
+if models.storage == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id', onupdate='CASCADE'
+                                            ondelete='CASCADE'),
+                                 primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id', onupdate='CASCADE'
+                                            ondelete='CASCADE'),
+                                 primary_key=True))
 
 
 class Place(BaseModel, Base):
