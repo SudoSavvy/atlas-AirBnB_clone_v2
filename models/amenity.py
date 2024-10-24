3129c97f21e5e0a015cf08e70f003497b0d1b739
SudoSavvy <donovanjheape@gmail.com>
2024-10-24 16:57:19 -0500

    Updating code in amenity and place

--- models/amenity.py
+++ models/amenity.py
@@ -1,26 +1,11 @@
-#!/usr/bin/python3
-"""This is the amenity class"""
-import models
-from os import getenv
 from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.place import place_amenity
-
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    if models.storage == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    """Represents an Amenity for a MySQL database."""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
 
-    # Relationship with Place
-    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
+    # Many-to-Many relationship with Place
+    place_amenities = relationship('Place', secondary='place_amenity', viewonly=False)
