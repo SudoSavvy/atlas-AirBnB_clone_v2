0d8f2393228c928af1d41aa2016b0253f60139f9
SudoSavvy <donovanjheape@gmail.com>
2024-10-24 16:21:52 -0500

    Updating unittests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,37 +1,19 @@
 #!/usr/bin/python3
-import unittest
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.state import State
-from models import storage
-import MySQLdb
 
-class TestState(unittest.TestCase):
 
-    @unittest.skipIf(type(storage).__name__ != 'DBStorage', "DBStorage test only")
-    def test_create_state_db(self):
-        """Test state creation in DBStorage"""
-        db = MySQLdb.connect(host="localhost", user="hbnb_test", passwd="hbnb_test_pwd", db="hbnb_test_db")
-        cursor = db.cursor()
+class test_state(test_basemodel):
+    """ """
 
-        # Check state count before creation
-        cursor.execute("SELECT COUNT(*) FROM states;")
-        count_before = cursor.fetchone()[0]
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
 
-        # Create state
-        state = State(name="California")
-        state.save()
-
-        # Check state count after creation
-        cursor.execute("SELECT COUNT(*) FROM states;")
-        count_after = cursor.fetchone()[0]
-
-        self.assertEqual(count_after, count_before + 1)
-        cursor.close()
-        db.close()
-
-    @unittest.skipIf(type(storage).__name__ != 'FileStorage', "FileStorage test only")
-    def test_create_state_file(self):
-        """Test state creation in FileStorage"""
-        state = State(name="California")
-        state.save()
-        states = storage.all(State)
-        self.assertIn(state, states.values())
+    def test_name3(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,46 +1,34 @@
-#!/usr/bin/python3import unittest
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.user import User
-from models import storage
-import MySQLdb
 
-class TestUser(unittest.TestCase):
 
-    @unittest.skipIf(type(storage).__name__ != 'DBStorage', "DBStorage test only")
-    def test_create_user_db(self):
-        """Test user creation in DBStorage"""
-        db = MySQLdb.connect(host="localhost", user="hbnb_test", passwd="hbnb_test_pwd", db="hbnb_test_db")
-        cursor = db.cursor()
+class test_User(test_basemodel):
+    """ """
 
-        # Check user count before creation
-        cursor.execute("SELECT COUNT(*) FROM users;")
-        count_before = cursor.fetchone()[0]
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "User"
+        self.value = User
 
-        # Create user
-        user = User(email="test@user.com", password="test_pwd")
-        user.save()
+    def test_first_name(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.first_name), str)
 
-        # Check user count after creation
-        cursor.execute("SELECT COUNT(*) FROM users;")
-        count_after = cursor.fetchone()[0]
+    def test_last_name(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.last_name), str)
 
-        self.assertEqual(count_after, count_before + 1)
-        cursor.close()
-        db.close()
+    def test_email(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.email), str)
 
-    @unittest.skipIf(type(storage).__name__ != 'FileStorage', "FileStorage test only")
-    def test_create_user_file(self):
-        """Test user creation in FileStorage"""
-        user = User(email="test@user.com", password="test_pwd")
-        user.save()
-        users = storage.all(User)
-        self.assertIn(user, users.values())
-
-    def test_create_user_missing_email(self):
-        """Test user creation fails with missing email"""
-        with self.assertRaises(ValueError):
-            User(password="test_pwd").save()
-
-    def test_create_user_missing_password(self):
-        """Test user creation fails with missing password"""
-        with self.assertRaises(ValueError):
-            User(email="test@user.com").save()
+    def test_password(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.password), str)
