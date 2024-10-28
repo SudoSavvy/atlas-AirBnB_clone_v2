48b05ad34de8c3ee91d430612a8d5fd699d11655
SudoSavvy <donovanjheape@gmail.com>
2024-10-28 18:36:37 -0500

    Updating code in file storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,10 @@
 """ base class for all predetermined models """
 from os import getenv
 from datetime import datetime
-import uuid
 import models
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from uuid import uuid4
 
 
 TIME = "%Y-%m-%dT%H:%M:%S.%f"
@@ -13,16 +13,17 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """ base class for all models defined """
+    """ Base class for all models defined. """
     id = Column(String(60), nullable=False, primary_key=True, unique=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """ starts up base object and initializes it """
+        """ Initializes a new base object. """
         self.id = kwargs.get('id', str(uuid4()))
         self.created_at = kwargs.get('created_at', datetime.utcnow())
         self.updated_at = kwargs.get('updated_at', self.created_at)
+
         if kwargs:
             for key, value in kwargs.items():
                 if key in ["created_at", "updated_at"]:
