1066465d4872b14a5a139d3e0bbb119d3835d70f
SudoSavvy <donovanjheape@gmail.com>
2024-10-28 17:37:11 -0500

    Updating code in file storage

--- models/city.py
+++ models/city.py
@@ -9,9 +9,10 @@ from models.place import Place
 
 
 class City(BaseModel, Base):
-    """ class that represents City and inheriteds """
-    __tablename__ = "cities"
+    """Represents a city for a MySQL database."""
+    __tablename__ = 'cities'
+
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+
+    places = relationship("Place", backref="city", cascade='all, delete, delete-orphan')
