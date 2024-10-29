fc60f7bca67a43095d376c687d881dab0fc63def
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-10-29 14:31:31 -0500

    date format

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,8 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key in ["created_at", "updated_at"]:
-                    date_value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    date_format = "%Y-%m-%dT%H:%M:%S.%f"
+                    date_value = datetime.strptime(value, date_format)
                     setattr(self, key, date_value)
                 elif key != '__class__':
                     setattr(self, key, value)
--- models/place.py
+++ models/place.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 place_amenity = Table(
-    'place_amenity', 
-    Base.metadata,   
+    'place_amenity',
+    Base.metadata,
     Column('place_id', String(60), ForeignKey('places.id'),
            primary_key=True, nullable=False),
     Column('amenity_id', String(60), ForeignKey('amenities.id'),
