9ffd4a367b50fc5c198f70e8e3c3eb772f77d459
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-10-29 06:43:58 -0500

    updated modules with updated typeset and org

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,12 @@
+#!/usr/bin/python3
+""" amenity module """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class Amenity(BaseModel):
+
+class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
-    id = Column(String(60), primary_key=True)
     name = Column(String(128), nullable=False)
-
-    places = relationship("Place", secondary="place_amenity", back_populates="amenities")
-
+    place_amenities = relationship('Place', secondary='place_amenity',
+                                   viewonly=False)
--- models/place.py
+++ models/place.py
@@ -1,18 +1,68 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, String, ForeignKey, Table
-from sqlalchemy.orm import relationship
+""" place module """
+import models
 from models.base_model import BaseModel, Base
+from models.review import Review
+from models.amenity import Amenity
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from os import getenv
 
 place_amenity = Table('place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False),
-    extend_existing=True
-)
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
-class Place(BaseModel):
+
+class Place(BaseModel, Base):
+    """ class that handles Place ids and many many handling """
     __tablename__ = 'places'
-    id = Column(String(60), primary_key=True)
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
 
-    amenities = relationship("Amenity", secondary="place_amenity", back_populates="places")
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            """ file storage getter """
+            reviews_list = []
 
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    reviews_list.append(review)
+            return reviews_list
+
+        @property
+        def amenities(self):
+            """ getter run """
+            amenities_list = []
+
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenities_list.append(amenity)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """ append function for creation """
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
+    else:
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete, delete-orphan')
+        amenities = relationship("Amenity",
+                                 secondary='place_amenity',
+                                 back_populates='place_amenities',
+                                 viewonly=False)
