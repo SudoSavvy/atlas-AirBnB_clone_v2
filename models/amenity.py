f30fdb406ddb2c2cddb36b142c52d31ac702c1b8
SudoSavvy <donovanjheape@gmail.com>
2024-10-28 21:11:15 -0500

    Updating code in file storage

--- models/amenity.py
+++ models/amenity.py
@@ -3,9 +3,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database."""
     __tablename__ = 'amenities'
+    
     name = Column(String(128), nullable=False)
-
-    # Many-to-Many relationship with Place
-    place_amenities = relationship('Place', secondary='place_amenity', viewonly=False)
+    place_amenities = relationship("Place", secondary="place_amenity", back_populates="amenities")
