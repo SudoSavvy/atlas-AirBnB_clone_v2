d4bc7077397fd54ecb667b373b7fad8138c5eb28
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-10-29 12:40:05 -0500

    module pycode

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,12 @@
+#!/usr/bin/python3
+""" amenity class handler """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database."""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-
-    # Many-to-Many relationship with Place
-    place_amenities = relationship('Place', secondary='place_amenity', viewonly=False)
+    place_amenities = relationship('Place', secondary='place_amenity',
+                                   viewonly=False)
