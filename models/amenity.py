6261908ce08c6e4665d743b8f3f6467e59acb4ec
SudoSavvy <donovanjheape@gmail.com>
2024-10-29 08:35:09 -0500

    Pushing hopefully working version of repo

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import relationship
 
 place_amenity = Table('place_amenity', Base.metadata,
     Column('place_id', String(60), ForeignKey('places.id'), primary_key=True),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True)
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True),
+    extend_existing=True
 )
 class Amenity(BaseModel):
     __tablename__ = 'amenities'
