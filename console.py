f2d54f5a0f70df903e856153d4a3c7888c77983c
SudoSavvy <donovanjheape@gmail.com>
2024-10-28 20:43:33 -0500

    Updating code in file storage

--- console.py
+++ console.py
@@ -112,24 +112,43 @@ class HBNBCommand(cmd.Cmd):
     import re
 
     def do_create(self, arg):
-        """Create a new instance of a class and save it to JSON file"""
-        if not arg:
+        args = arg.split()
+        if not args:
             print("** class name missing **")
             return
-        if arg not in self.classes:
+
+        class_name = args[0]
+        if class_name not in classes:
             print("** class doesn't exist **")
             return
-        
-        # Create an instance of the specified class
-        obj = self.classes[arg]() 
-        obj_id = obj.id  # Store the ID for printing
 
-        # Save the object to storage
-        storage.new(obj)
+        # Instantiate the new object
+        new_instance = classes[class_name]()
+
+        # Process additional attributes from the input
+        for param in args[1:]:
+            if "=" in param:
+                key, value = param.split("=", 1)
+                if value.startswith('"') and value.endswith('"'):
+                    value = value[1:-1].replace("_", " ")
+                elif "." in value:
+                    try:
+                        value = float(value)
+                    except ValueError:
+                        continue
+                else:
+                    try:
+                        value = int(value)
+                    except ValueError:
+                        continue
+                setattr(new_instance, key, value)
+
+        # Add and save the new instance to ensure it's in file.json
+        storage.new(new_instance)
         storage.save()
 
-        # Print the ID of the new instance as per your requirements
-        print(obj_id)
+        # Print the new ID as expected
+        print(new_instance.id)
 
 
     def help_create(self):
