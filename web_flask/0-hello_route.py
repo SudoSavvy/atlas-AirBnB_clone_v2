f504b3f36b08656a3065c4a9b8bb8d075ef6105b
SudoSavvy <donovanjheape@gmail.com>
2024-10-31 16:57:38 -0500

    Flask project work

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""
+A Flask web application that displays 'Hello HBNB!' at the root route.
+
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!' on the root route."""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
+    # Runs the application on host 0.0.0.0, port 5000
     app.run(host="0.0.0.0", port=5000)
