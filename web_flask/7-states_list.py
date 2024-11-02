247138516a45919578a03302631a4cb3cdb0a4b2
SudoSavvy <donovanjheape@gmail.com>
2024-11-02 14:37:43 -0500

    Flask project work

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-"""
-Module: 7-states_list
-Description: Starts a Flask web application to display a list of all State
-             objects from a database, sorted by name.
-"""
+"""Starts a Flask web application that displays a list of states"""
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close the storage session after each request."""
+    """Close the storage session"""
     storage.close()
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a HTML page with a list of all State objects."""
-    states = storage.all("State")
-    sorted_states = sorted(states.values(), key=lambda x: x.name)  # Sort states by name
+    """Display a HTML page with a list of states"""
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
 
 if __name__ == "__main__":
